55e915f960d23f9c3a013e0a0471a5cff370726d
Eduardo Marra <eduardomarra@gmail.com>
2023-08-29 23:23:28 -0300

    fix: package name fix

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@emarra-ui/react'
 import { ArrowRight } from 'phosphor-react'
 
 export default {
